63b440fa6e20a05bfc5b0b6916c1e5f4000fea1d
marchly2020 <pawanja_r@silpakorn.edu>
2022-09-16 09:36:57 +0700

    first commit

---
+++ src/main/java/com/example/springbootfacebookloginexample/SpringBootFacebookLoginExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfacebookloginexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFacebookLoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFacebookLoginExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootfacebookloginexample/SpringBootFacebookLoginExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springbootfacebookloginexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+
+@SpringBootTest
+class SpringBootFacebookLoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
